fadf58ab771ecd4850f55a2c906b412efe68daa5
matyushkin-s <alxmat94@gmail.com>
2023-07-10 20:48:24 +0300

    Commited lesson2 before hooks started

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import Button from './components/Button';
 import Input from './components/Input';
 import './App.css';
+// import List from './components/List';
 
 function App() {
+  const buttonText = "Click here!";
+  // const users = ['User1', 'User2', 'User3', 'User4'];
+  const disabled = true;
+
+  const handleSave = (e) => {
+    console.log('Save it!');
+  }
+
+  const handleSubmit = (e) => {
+    console.log('Submit it!');
+  }
+
   return (
     <div className="App">
       <h1>Hello world!</h1>
       <Input
         placeholder="Write something here..."
       />
+      <Button text={buttonText} isDisabled={disabled}/>
       <br></br>
-      <Button title="Click" text="Click 1" />
+      <Button>{buttonText}</Button>
 
-      <Button title="Test title" text="Click " />
+      <Button text="Save" onClick={handleSave} />
+
+      <Button text="Submit" onClick={handleSubmit} />
+
+      <div>
+        {/* <List items={users} /> */}
+      </div>
     </div>
   );
 }
