0e672b8e414bd873db3855408646b2dd8930009a
matyushkin-s <alxmat94@gmail.com>
2023-07-10 19:03:39 +0300

    Init first react.js project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Button from './components/Button';
+import Input from './components/Input';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world!</h1>
+      <Input />
+      <br></br>
+      <Button text="Click 1" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Input.jsx
@@ -0,0 +1,7 @@
+const Input = () => {
+  return (
+    <input type="text" placeholder="Search..." />
+  )
+}
+
+export default Input
