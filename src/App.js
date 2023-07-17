05a44c2d75ad5bc7a88144a4c3d6e1b7dee1b076
matyushkin-s <alxmat94@gmail.com>
2023-07-18 00:01:50 +0300

    Added HW2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-// import Button from './components/Button';
-// import Input from './components/Input';
+import React, { useState } from 'react'
+import Button from './components/Button';
 import './App.css';
-// import List from './components/List';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const increase = () => {
+    setCounter(count => count + 1);
+  };
+
+  const decrease = () => {
+    setCounter(count => count - 1);
+  };
+
+  const reset = () =>{
+    // console.log('RESET');
+    setCounter(0)
+  }
+
   return (
     <div className="App">
-      <h1>Hello world!</h1>
-      
-      <div>
+      <div className="counter">
+        <h1 className="counter__header">It`s counter here</h1>
+        <div className="counter__value">Counter: { counter }</div>
 
+        <div className="counter__container">
+          <Button class="control__btn" onClick={increase}>+</Button>
+          <Button class="control__btn" onClick={decrease}>-</Button>
+        </div>
+
+        <Button
+          class="reset__btn"
+          isDisabled={counter === 0}
+          onClick={reset}
+        >
+          Reset
+        </Button>
       </div>
     </div>
   );
