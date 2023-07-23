d5ffb1783a8b900fd3cf48644675d2d8f8726834
matyushkin-s <alxmat94@gmail.com>
2023-07-23 15:13:49 +0300

    Added Router to Project

--- src/App.js
+++ src/App.js
@@ -1,42 +1,16 @@
-import React, { useState } from 'react'
-import Button from './components/Button';
+import { Route, Router } from 'react-router-dom'
+import React from 'react'
+// import 
 import './App.css';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  const increase = () => {
-    setCounter(count => count + 1);
-  };
-
-  const decrease = () => {
-    setCounter(count => count - 1);
-  };
-
-  const reset = () =>{
-    // console.log('RESET');
-    setCounter(0)
-  }
-
   return (
     <div className="App">
-      <div className="counter">
-        <h1 className="counter__header">It`s counter here</h1>
-        <div className="counter__value">Counter: { counter }</div>
+      <Router>
+        <Route>
 
-        <div className="counter__container">
-          <Button class="control__btn" onClick={increase}>+</Button>
-          <Button class="control__btn" onClick={decrease}>-</Button>
-        </div>
-
-        <Button
-          class="reset__btn"
-          isDisabled={counter === 0}
-          onClick={reset}
-        >
-          Reset
-        </Button>
-      </div>
+        </Route>
+      </Router>
     </div>
   );
 }
